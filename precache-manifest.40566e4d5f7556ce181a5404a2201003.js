2c0b6f6c721d44658712d407368d94ec8248374b
Lucifer <ramanandsirvi21@gmail.com>
2019-07-11 20:34:38 +0530

    Updates

---
+++ precache-manifest.40566e4d5f7556ce181a5404a2201003.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65850af981367a2f08d392f424fcdc1",
+    "url": "/chat/index.html"
+  },
+  {
+    "revision": "ac3662a2cecc11b75c24",
+    "url": "/chat/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "8f2823bf434bb591af11",
+    "url": "/chat/static/js/2.d4782eea.chunk.js"
+  },
+  {
+    "revision": "ac3662a2cecc11b75c24",
+    "url": "/chat/static/js/main.fc2ff585.chunk.js"
+  },
+  {
+    "revision": "5bb637843dac5d879ad5",
+    "url": "/chat/static/js/runtime~main.54cd968c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/chat/static/media/logo.5d5d9eef.svg"
+  }
+]);
